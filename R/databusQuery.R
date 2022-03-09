03d95635e6c039999ec9118300f9a73d20a4515b
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2022-03-09 10:49:11 +0100

    lint and version bump

--- R/databusQuery.R
+++ R/databusQuery.R
@@ -14,7 +14,7 @@ databusQuery <- function(url = "https://databus.dbpedia.org/repo/sparql", q = NU
                          metadata = "emissiondata") {
 
 
-  if (is.null(q)) q<- "
+  if (is.null(q)) q <- "
   PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>
   PREFIX dct:    <http://purl.org/dc/terms/>
   PREFIX dcat:   <http://www.w3.org/ns/dcat#>
@@ -22,13 +22,13 @@ databusQuery <- function(url = "https://databus.dbpedia.org/repo/sparql", q = NU
   PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>
   SELECT DISTINCT ?file ?g WHERE {
-	  GRAPH ?g {
-		  ?dataset dcat:distribution ?distribution .
-		  ?distribution dataid:file ?file .
+    GRAPH ?g {
+      ?dataset dcat:distribution ?distribution .
+      ?distribution dataid:file ?file .
 
 
-		  { ?distribution <dataid-cv:type> 'METADATASTRING'^^<http://www.w3.org/2001/XMLSchema#string> . }
-	  }
+      { ?distribution <dataid-cv:type> 'METADATASTRING'^^<http://www.w3.org/2001/XMLSchema#string> . }
+    }
   }
   "
   return(SPARQL(url, sub("METADATASTRING", metadata, q)))
