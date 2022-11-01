95bd75612a1eed8364db66d5df0202796556786d
Michaja Pehl <pehl@pik-potsdam.de>
2022-11-01 16:51:13 +0100

    ignore vignette output

--- vignettes/databusUpload.R
+++
@@ -1,5 +0,0 @@
-## ----setup, include=FALSE-----------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-
-## ---- eval = FALSE------------------------------------------------------------
-#  r2databus::databusUpload(myKey = "a-s3cr3t-K3y", myData = "/path/to/JSON/file") # nolint
